379d75d0e99cbcc8473765db8456cfb85a4b4e2c
xWIREHEADx <mattadamosart@gmail.com>
2023-11-23 03:47:20 -0500

    Banking App & Main Menu defined

--- BankAppFinal.py
+++ BankAppFinal.py
@@ -52,3 +52,48 @@ class Bank:
     
         self.accounts.append(new_account)
         return new_account
+
+class BankApp:
+    def __init__(self):
+        self.bank = Bank()
+        self.current_account = None
+
+    def showMainMenu(self):
+        while True:
+            print("\nMAIN MENU")
+            print("1. Select Account")
+            print("2. Open Account")
+            print("3. Exit")
+
+            Selection = input("Welcome! Please select what you would like to do today: ")
+
+            if Selection == "1":
+                account_number = input("Enter the account number: ")
+                self.current_account = self.bank.searchAccount(account_number)
+
+                if self.current_account:
+                    self.showAccountMenu()
+                else:
+                    print("We're sorry, you're account cannot be found.")
+
+            elif Selection == "2":
+                account_type = input("Please enter your account type (Savings/Chequing): ")
+                account_number = input("Enter account number: ")
+                initial_balance = float(input("Enter your initial balance: "))
+                parameters = float(input("Enter your minimum balance or overdraft limit: "))
+
+                new_account = self.bank.openAccount(account_type, account_number, initial_balance, parameters)
+
+                if new_account:
+                    print(f"Your account {account_number} has successfully opened!")
+                    self.current_account = new_account
+                    self.showAccountMenu()
+                else:
+                    print("Invalid account! Please choose either Savings or Chequing.")
+
+            elif Selection == "3":
+                print("Exiting now. Have a good day!")
+                break
+
+            else:
+                print("Invalid selection! Please try again.")
