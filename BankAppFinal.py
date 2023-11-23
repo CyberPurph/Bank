037c960864bb2cd06fa0e85fae0f4f6ffdf62237
xWIREHEADx <mattadamosart@gmail.com>
2023-11-23 03:12:30 -0500

    Account class created

---
+++ BankAppFinal.py
@@ -0,0 +1,13 @@
+class Account:
+    def __init__(self, account_number, balance=0):
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        pass
+    
+
+             
